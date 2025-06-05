34cf3bf163bd59163be7aa69be9ee7e1d78a95fb
leo42night <karmaborutovvo@gmail.com>
2025-06-06 03:37:22 +0700

    Soekarno PUSH to Repo Dir TFJS 1749155842

--- Dockerfile
+++ Dockerfile
@@ -8,11 +8,6 @@ WORKDIR /etl
 RUN apt-get update && \
 apt-get install -y --no-install-recommends git openssh-client && \
 rm -rf /var/lib/apt/lists/*
-# RUN apt-get update && \
-#     apt-get install -y git && \
-#     apt-get clean && \
-#     rm -rf /var/lib/apt/lists/*
-
 
 # 4. Salin file dependency
 COPY requirements-test.txt requirements-full.txt
--- full-etl.py
+++ full-etl.py
@@ -1,220 +1,241 @@
-from flask import Flask
+from flask import Flask, request
+import logging
 import os
+os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
+from dotenv import load_dotenv
+import time
 import subprocess
+import json
 import requests
-from IPython.display import display
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
+# import tensorflow as tf
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Embedding, GlobalAveragePooling1D, Dense
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Embedding, GlobalAveragePooling1D, Dense
 from tensorflow.keras.utils import to_categorical
 
 app = Flask(__name__)
+app.logger.setLevel(logging.DEBUG)
 
-TOKEN_GITHUB = os.getenv("GITHUB_TOKEN", "")
-
-@app.route('/')
-def home():
-    return "ETL PROCESS HISTOTALK MODEL 1/TFJS"
-
-@app.route('/etl-run-model1', methods=['POST'])
-def etlRun():
-    # -- SETUP -- pindah ke folder induk repo
-    # os.chdir("./") # sesuaikan dengan lingkungan aplikasi
+load_dotenv()  # ⬅️ Ini penting agar .env dibaca
+GITHUB_TOKEN = os.getenv("GITHUB_TOKEN", "")
+ETL_KEY = os.getenv("ETL_KEY")
     
-    # ⛅ Input informasi
-    username = "leo42night"  # akun GitHub kamu yang punya akses
-    org = "DBS-Coding"
-    repo = "histotalk-model1-tfjs"
-    email = "karmaborutovvo@gmail.com"
-    branch = "main"
+# Tempat menyimpan file model
+username = "leo42night"
+org = "DBS-Coding"
+repo = "histotalk-model1-tfjs"
+email = "karmaborutovvo@gmail.com"
+branch = "main"
 
-    # URL dengan token
-    repo_url = f"https://{username}:{TOKEN_GITHUB}@github.com/{org}/{repo}.git"
+# URL
+repo_url = f"https://{username}:{GITHUB_TOKEN}@github.com/{org}/{repo}.git"
+dataset_url = "https://capstone-five-dusky.vercel.app/chatbot/tags/nama"
 
+
+def getRepo():
     if os.path.isdir(repo):
-        print("Folder repo ada, pindah ke dalam repo!")
-        os.chdir(repo)
+        app.logger.info(f"Folder repo {repo} ada, pindah ke dalam repo!")
+        os.chdir(repo) #masuk ke dalam karena ada git fetch
+        app.logger.info(f"Current directory: {os.getcwd()}")
         
         # fetching
-        subprocess.run(["git", "fetch", "origin", branch])
+        subprocess.run(["git", "fetch", "origin", branch], check=True)
         # sesuaikan dengan data repo agar tidak ada conflict
-        subprocess.run(["git", "reset", "--hard", "origin/main"])
+        subprocess.run(["git", "reset", "--hard", "origin/main"], check=True)
+        
+        os.chdir("../")  # Kembali ke folder induk
+        app.logger.info(f"Current directory: {os.getcwd()}")
+        
     else:
-        print("Folder repo tidak ada, CLONE REPO!!")
+        app.logger.info("Folder repo tidak ada, CLONE REPO!!")
         # Konfigurasi Git global
         subprocess.run(["git", "config", "--global", "user.email", email], check=True)
         subprocess.run(["git", "config", "--global", "user.name", username], check=True)
 
         # Clone dari organization
-        subprocess.run(["git", "clone", "-b", branch, repo_url])
-    
-        # masuk ke direktori repo
-        os.chdir(repo)
-        
-        
-    # --- PERUBAHAN ---
-    # --- pull Dataset dari Database ---
-    url1 = "https://capstone-five-dusky.vercel.app/chatbot/tags/nama/Soekarno"
-    url2 = "https://capstone-five-dusky.vercel.app/chatbot/tags/nama/hatta"
-    # {'code': 200, 'status': 'success', 'message': '', 'data': [{'tag': 'nationalism', 'nama': 'Soekarno', 'input': [], 'responses': []
+        subprocess.run(["git", "clone", "-b", branch, repo_url], check=True)
+
+
+def modelling(npc):
     try:
-        response1 = requests.get(url1)
-        response2 = requests.get(url2)
-        response1.raise_for_status()  # Akan raise error jika status bukan 200
-        response2.raise_for_status()
-
-        data1 = response1.json() # Jika response-nya adalah JSON
-        data2 = response2.json()
-        print(data1)
-        print(data2)
-
+        response = requests.get(f"{dataset_url}/{npc}")
+        response.raise_for_status()  # Akan raise error jika status bukan 200
+        data = response.json() # Jika response-nya adalah JSON
+        app.logger.info("data didapatkan!")
+        
     except requests.exceptions.RequestException as e:
-        print("Terjadi kesalahan:", e)
+        app.logger.error(f"Terjadi kesalahan: {e}")
+        return
     
-    # tampikan data nama NPC
-    print(data1["data"][0]["nama"])
-    print(data2["data"][0]["nama"])
+    app.logger.info(data["data"][0]["nama"]) # tampikan data nama NPC
     
     # getting all the data list
     # soekarno
-    tags1 = []
-    inputs1 = []
-    responses1 = {}
-    for intent in data1['data']:
-        responses1[intent['tag']] = intent['responses']
+    tags = []
+    inputs = []
+    responses = {}
+    for intent in data['data']:
+        responses[intent['tag']] = intent['responses']
         for lines in intent['input']:
-            inputs1.append(lines.lower())
-            tags1.append(intent['tag'])
-    display(len(list(set(tags1))))
-    print("tags1: ", tags1)
-    print("inputs1: ", inputs1)
-    print("responses1: ", responses1)
-
-    # hatta
-    tags2 = []
-    inputs2 = []
-    responses2 = {}
-    for intent in data1['data']:
-        responses2[intent['tag']] = intent['responses']
-        for lines in intent['input']:
-            inputs2.append(lines.lower())
-            tags2.append(intent['tag'])
-    display(len(list(set(tags2))))
-    print("tags2: ", tags2)
-    print("inputs2: ", inputs2)
-    print("responses2: ", responses2)
-
+            inputs.append(lines.lower())
+            tags.append(intent['tag'])
+    app.logger.info(len(list(set(tags))))
+    app.logger.info(f"tags: {tags}")
+    
     # 1. export into dataframe
-    df1 = pd.DataFrame({"inputs":inputs1, "tags":tags1})
-    display(df1.head())
-    df2 = pd.DataFrame({"inputs":inputs2, "tags":tags2})
-    display(df2.head())
+    df = pd.DataFrame({"inputs":inputs, "tags":tags})
+    app.logger.info(df.head().to_string())
     
     # 2. Encode label ke angka
-    label_encoder1 = LabelEncoder()
-    df1['label'] = label_encoder1.fit_transform(df1['tags'])
-    num_classes1 = len(label_encoder1.classes_)
-
-    label_encoder2 = LabelEncoder()
-    df2['label'] = label_encoder2.fit_transform(df2['tags'])
-    num_classes2 = len(label_encoder2.classes_)
+    label_encoder = LabelEncoder()
+    df['label'] = label_encoder.fit_transform(df['tags'])
+    num_classes = len(label_encoder.classes_)
+    app.logger.info(f"Number of classes: {num_classes}")
 
     # 3. Split data
-    X_train1, X_test1, y_train1, y_test1 = train_test_split(df1['inputs'], df1['label'], test_size=0.2, random_state=42, stratify=df1['label'])
-    X_train2, X_test2, y_train2, y_test2 = train_test_split(df2['inputs'], df2['label'], test_size=0.2, random_state=42, stratify=df2['label'])
+    X_train, X_test, y_train, y_test = train_test_split(df['inputs'], df['label'], test_size=0.2, random_state=42, stratify=df['label'])
 
     # 4. Tokenisasi
-    tokenizer1 = Tokenizer(oov_token="<OOV>")
-    tokenizer1.fit_on_texts(X_train1)
-    word_index1 = tokenizer1.word_index
-
-    tokenizer2 = Tokenizer(oov_token="<OOV>")
-    tokenizer2.fit_on_texts(X_train2)
-    word_index2 = tokenizer2.word_index
-
+    tokenizer = Tokenizer(oov_token="<OOV>")
+    tokenizer.fit_on_texts(X_train)
+    word_index = tokenizer.word_index
+    
     # 5. Konversi teks ke urutan angka
-    train_sequences1 = tokenizer1.texts_to_sequences(X_train1)
-    test_sequences1 = tokenizer1.texts_to_sequences(X_test1)
-
-    train_sequences2 = tokenizer2.texts_to_sequences(X_train2)
-    test_sequences2 = tokenizer2.texts_to_sequences(X_test2)
+    train_sequences = tokenizer.texts_to_sequences(X_train)
+    test_sequences = tokenizer.texts_to_sequences(X_test)
 
     # 6. Padding
     # max_length = max([len(x) for x in train_sequences]) # otomatis
     max_length = 10 # statis mengikuti standar 10 classes (karena isue tfjs predict)
-    X_train_padded1 = pad_sequences(train_sequences1, maxlen=max_length, padding='post')
-    X_test_padded1 = pad_sequences(test_sequences1, maxlen=max_length, padding='post')
-
-    X_train_padded2 = pad_sequences(train_sequences2, maxlen=max_length, padding='post')
-    X_test_padded2 = pad_sequences(test_sequences2, maxlen=max_length, padding='post')
+    X_train_padded = pad_sequences(train_sequences, maxlen=max_length, padding='post')
+    X_test_padded = pad_sequences(test_sequences, maxlen=max_length, padding='post')
 
     # 7. One-hot encode label
-    y_train_cat1 = to_categorical(y_train1, num_classes=num_classes1)
-    y_test_cat1 = to_categorical(y_test1, num_classes=num_classes1)
-
-    y_train_cat2 = to_categorical(y_train2, num_classes=num_classes2)
-    y_test_cat2 = to_categorical(y_test2, num_classes=num_classes2)
+    y_train_cat = to_categorical(y_train, num_classes=num_classes)
+    y_test_cat = to_categorical(y_test, num_classes=num_classes)
 
     # 8. Buat model
-    model1 = Sequential([
-        Embedding(input_dim=len(word_index1) + 1, output_dim=16, input_length=max_length),
-        GlobalAveragePooling1D(),
-        Dense(16, activation='relu'),
-        Dense(num_classes1, activation='softmax')
-    ])
-    model1.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model_inputs = Input(shape=(max_length,))  # fleksibel batch size
+    x = Embedding(input_dim=len(word_index)+1, output_dim=16)(model_inputs)
+    x = GlobalAveragePooling1D()(x)
+    x = Dense(16, activation='relu')(x)
+    outputs = Dense(num_classes, activation='softmax')(x)
 
-    model2 = Sequential([
-        Embedding(input_dim=len(word_index2) + 1, output_dim=16, input_length=max_length),
-        GlobalAveragePooling1D(),
-        Dense(16, activation='relu'),
-        Dense(num_classes2, activation='softmax')
-    ])
-    model2.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model = Model(inputs=model_inputs, outputs=outputs)
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
     # 9. Latih model
-    history1 = model1.fit(X_train_padded1, y_train_cat1, epochs=50, validation_data=(X_test_padded1, y_test_cat1), verbose=2)
-    history2 = model2.fit(X_train_padded2, y_train_cat2, epochs=50, validation_data=(X_test_padded2, y_test_cat2), verbose=2)
+    model.fit(X_train_padded, y_train_cat, epochs=50, validation_data=(X_test_padded, y_test_cat), verbose=2)
 
     # 10. Evaluasi
-    loss1, accuracy1 = model1.evaluate(X_test_padded1, y_test_cat1)
-    print(f"Test Accuracy (1): {accuracy1:.2f}")
+    loss, accuracy = model.evaluate(X_test_padded, y_test_cat)
+    app.logger.info(f"Test Accuracy: {accuracy:.2f}")
+    app.logger.info(f"Test Loss: {loss:.2f}")
 
-    loss2, accuracy2 = model2.evaluate(X_test_padded2, y_test_cat2)
-    print(f"Test Accuracy (2): {accuracy2:.2f}")
-    # dir yang akan di update
-    path = "tfjs_saved_model"
-
-    # Hapus folder jika ada
-    if os.path.exists(path):
-        # shutil.rmtree(path) # matikan untuk sementara
-        print(f"✅ Folder '{path}' berhasil dihapus.")
-    else:
-        print(f"⚠️ Folder '{path}' tidak ditemukan.")
+    # 11. Simpan model
+    # tf.saved_model.save(model, f'saved_model_{npc}')
+    model.export(f'saved_model_{npc}')
     
-    # ---- PUSH KE GITHUB ----
-    def run_cmd(cmd):
-        result = subprocess.run(cmd, capture_output=True, text=True)
-        print(f"\n👉 Perintah: {' '.join(cmd)}")
-        print("✅ STDOUT:")
-        print(result.stdout)
-        print("❌ STDERR:")
-        print(result.stderr)
-        if result.returncode != 0:
-            print(f"⚠️ Perintah gagal dengan kode {result.returncode}")
-        return result
-
-    # Tambah, commit, push
-    run_cmd(["git", "add", "."])
-    run_cmd(["git", "commit", "-m", "testing push"])
-    run_cmd(["git", "push", "origin", branch])
+    # -- TFJS CONVERTER --
+    # Definisikan perintah
+    cmd = [
+        "tensorflowjs_converter",
+        "--input_format=tf_saved_model",
+        "--output_format=tfjs_graph_model",
+        "--signature_name=serving_default",
+        "--saved_model_tags=serve",
+        f"saved_model_{npc}",
+        f"{repo}/tfjs_saved_model/{npc}"
+    ]
     
-    return "--> Push ETL Dijalankan <--"
+    try:
+        result = subprocess.run(cmd, capture_output=True, text=True, check=True)
+        app.logger.info(f"✅ STDOUT:\n{result.stdout}")
+        app.logger.info(f"❌ STDERR:\n{result.stderr}")
+        app.logger.info("✅ Konversi Model TFJS Soekarno berhasil")
+    except subprocess.CalledProcessError as e:
+        app.logger.error(f"⚠️ Konversi gagal dengan error: {e}")
+    except FileNotFoundError:
+        app.logger.error("⚠️ tensorflowjs_converter tidak ditemukan. Pastikan sudah terinstall dan PATH sudah benar.")
+
+    # -- SIMPAN ATRIBUT DATA MODEL --
+    # simpan word_index dan label_encoder
+    with open(f"{repo}/tfjs_saved_model/{npc}/word_index.json", "w") as f:
+        json.dump(word_index, f)
+        
+    # 1. Sort dataframe berdasarkan label
+    sorted_tags = df.sort_values('label')['tags'].drop_duplicates().tolist()
+    app.logger.info(sorted_tags)
+    # 2. Bangun dictionary baru dengan urutan sesuai label
+    sorted_responses = {tag: responses[tag] for tag in sorted_tags}
+    
+    with open(f"{repo}/tfjs_saved_model/{npc}/content.json", "w") as f:
+        json.dump(sorted_responses, f)
+
+
+def run_cmd(cmd, log_prefix):
+    result = subprocess.run(cmd, capture_output=True, text=True)
+    
+    if result.stdout:
+        app.logger.info(f"✅ {log_prefix} STDOUT:\n{result.stdout}")
+    if result.stderr:
+        app.logger.warning(f"❌ {log_prefix} STDERR:\n{result.stderr}")
+    
+    if result.returncode != 0:
+        app.logger.error(f"⚠️ Command failed with code {result.returncode}: {' '.join(cmd)}")
+    
+    return result
+
+def git_push(commit_message):
+    run_cmd(["git", "add", "."], "Git Add")
+    run_cmd(["git", "commit", "-m", commit_message], "Git Commit")
+    run_cmd(["git", "push", "origin", "main"], "Git Push")
+
+
+@app.route('/')
+def home():
+    return "ETL PROCESS HISTOTALK MODEL 1/TFJS"
+
+@app.route('/etl-run-model1-soekarno', methods=['POST'])
+def etlRunSoekarno():
+    data = request.get_json()
+    etl_key = data.get('etl_key') if data else None
+    
+    if not ETL_KEY or not etl_key or etl_key != ETL_KEY:
+        return "Unauthorized", 401
+    
+    getRepo() # {'code': 200, 'status': '', 'message': '', 'data': [{'tag': 'nationalism', 'nama': 'Soekarno', 'input': [], 'responses': []
+
+    modelling("soekarno") # run modelling
+    
+    # # push ke github
+    timestamp = int(time.time())  # Local time in seconds
+    git_push(f"Soekarno PUSH to Repo Dir TFJS {timestamp}")
+    
+    return "!! ETL Soekarno Dijalankan (DB -> Train TFJS -> Push GitHub) !!"
+
+@app.route('/etl-run-model1-hatta', methods=['POST'])
+def etlRunHatta():
+    app.logger.info("running etl-run-model1-hatta")
+    app.logger.info(f"ENV: {ETL_KEY}")
+    data = request.get_json()
+    etl_key = data.get('etl_key') if data else None
+    app.logger.info(f"json: {etl_key}")
+    
+    if not ETL_KEY or not etl_key or etl_key != ETL_KEY:
+        return "Unauthorized", 401
+    
+    getRepo() # clone repo dari github
+    modelling("hatta") # run modelling
+    timestamp = int(time.time())  # Local time in seconds
+    git_push(f"Hatta PUSH to Repo Dir TFJS {timestamp}")
+    
+    return "!! ETL Hatta Dijalankan (DB -> Train TFJS -> Push GitHub) !!"
 
 if __name__ == '__main__':
     port = int(os.getenv("PORT2", 8081))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=False) # hentikan fitur auto-reload dan auto-debug
